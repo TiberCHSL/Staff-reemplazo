bd9e965d181b86fe56d69d902998416a92fb2bfd
TiberCHSL <tibercarlitohsl@gmail.com>
2023-10-11 10:57:17 -0300

    cambio de BD

--- pagina/settings.py
+++ pagina/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'pagina.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': 'miwea',
+        'USER': 'loscabros',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '1521',
     }
 }
 
