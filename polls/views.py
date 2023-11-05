17052ffcb5a4d049b12b581fe09c7bd2b25ba6b6
TiberCHSL <tibercarlitohsl@gmail.com>
2023-11-05 13:45:34 -0300

    arregladas un par de direcciones locales, ahora se pueden usar en cualquier pc

--- polls/views.py
+++ polls/views.py
@@ -1,19 +1,22 @@
 from django.shortcuts import render
 from django.template import Template, Context
-
+import os
 from django.http import HttpResponse
 
+template_path = os.path.join('polls', 'Templates', 'login.html')
+template_path2 = os.path.join('polls', 'Templates', 'Registro.html')
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 def login(request):
-    login = open("D:/La_U/unap/FLP/pagina/polls/Templates/login.html")
-    template = Template(login.read())
-    login.close()
+    with open(template_path, 'r') as template_file:
+        template = Template(template_file.read())
     contexto = Context()
     documento = template.render(contexto)
     return HttpResponse(documento)
+
+
 
 def registro(request):
     registro = open("D:/La_U/unap/FLP/pagina/polls/Templates/Registro.html")
